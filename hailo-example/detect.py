878e06d13bf0e4dfa04e2c2bf50e6b156da75b4f
mikehailodev <michaelf@hailo.ai>
2026-04-14 12:21:58 +0300

    Fix group_id to match Frigate (SHARED)

--- hailo-example/detect.py
+++ hailo-example/detect.py
@@ -86,7 +86,7 @@ def main():
     params = VDevice.create_params()
     params.scheduling_algorithm = HailoSchedulingAlgorithm.ROUND_ROBIN
     params.multi_process_service = True
-    params.group_id = "shared"
+    params.group_id = "SHARED"
 
     vdevice = VDevice(params)
     logger.info("Connected to HailoRT service!")
